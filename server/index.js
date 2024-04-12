9a67171401a70f7c3516f7cd5f9664ac01040648
Parkstelth <stelthicmaster@gmail.com>
2024-04-12 19:47:20 +0900

    세팅2

--- server/index.js
+++ server/index.js
@@ -111,6 +111,6 @@ app.get("/api/users/logout", auth, (req, res) => {
   });
 });
 
-const port = 3000;
+const port = 5000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
